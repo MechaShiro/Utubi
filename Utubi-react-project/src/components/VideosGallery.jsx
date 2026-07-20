fe1e8f158d1d86f1e7d074a63a6c5e6e883e8cb5
CodeGoblin <nourstuff@gmail.com>
2026-07-20 19:02:03 +0100

    20-07

--- Utubi-react-project/src/components/VideosGallery.jsx
+++ Utubi-react-project/src/components/VideosGallery.jsx
@@ -1,45 +1,39 @@
 import '../css/videosGallery.css'
 
-function VideosGallery({fullWidth}){
+function VideosGallery({fullWidth , videos}){
 
-    async function fetchData () {
-        try{ 
-            const response = await fetch ("https://pokeapi.co/api/v2/pokemon");
 
-            if(!response.ok){
-                throw new Error("Could not fetch resource")
-            }
-
-            const data = await response.json();
-            console.log(data)
-        }
-        catch(error){
-            console.error(error)
-        }
-    }
+    
        
 
         return(
                 <>
                     <div className={fullWidth ? 'divTesteSquare' : 'divTesteSquare2'}>
-                        <h1>OI</h1>
-
-                        <div className='content'> card
-                                <img className='content_img_testeTB'></img> card_thumbnail
+                         
+                    {videos.map((video) => (
+                        <div className='card'> 
+                                <img 
+                                    className='card_thumbnail'
+                                    src={video.snippet.thumbnails.high.url}></img> 
                                 
-                                <div className='content__footer_teste'> card_footer
-                                    <img className='profile_img_testeTB'></img> card_footer___profilePic
-                                    <div className='paragraphs__content__footer_teste'> card_footer___text
-                                        <p className='pGrande'>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p> card_footer___text__title
-                                        <p className='pMedio'>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
-                                        <p className='pPequeno'>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
-                                    </div> 
+                                <div className='card_footer'>
+                                    <img 
+                                        className='card_footer___profilePic'
+                                        src={video.snippet.thumbnails.high.url}></img>
+                                    <div className='card_footer___text'> 
+                                        <p className='card_footer___text__title' >
+                                            {video.snippet.description}
+                                        </p>
+                                        <p className='card_footer___text__channel'>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+                                        <p className='card_footer___text__views'>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+                                    </div>
                                 </div>
-                            </div>
                         </div>
+                        ))}
+                    </div>
                     
                 </>
-        )
-    };
+        );
+}
 
 export default VideosGallery;
